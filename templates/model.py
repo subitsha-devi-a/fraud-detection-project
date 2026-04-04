b88b98720b36874231f28b21da6493b03bee3501
subitsha-devi-a <senthilsubi2k@gmail.com>
2026-04-04 23:06:13 +0530

    Create model.py

---
+++ templates/model.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+import pickle
+
+data = {
+    "click_time": [1,2,3,5,6,7,1,2],
+    "click_count": [1,2,3,1,1,2,4,5],
+    "is_fraud": [1,1,1,0,0,0,1,1]
+}
+
+df = pd.DataFrame(data)
+
+X = df[["click_time", "click_count"]]
+y = df["is_fraud"]
+
+model = LogisticRegression()
+model.fit(X, y)
+
+pickle.dump(model, open("model.pkl", "wb"))
+
+print("Model trained")
